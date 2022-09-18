aebcd86bcdb355fcb9ff698cafa711e1b8d5d766
W3NDO <wendonyang@gmail.com>
2022-09-18 15:16:35 +0300

    fix(main) databse not found error

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake hotwire:install
+bundle exec rake turbo:install:redis
+
